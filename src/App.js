81c6f16a340450df4d65e913f4e50870d9283270
francisbautista <fgcb05@gmail.com>
2019-03-06 22:48:42 +1100

    add post

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      postValues: ["It's a lovely day!", "Hello World!", "I'm lonely"],
+      newPostValue: "",
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div style={{marginBottom:"15px"}}>
+          <input type="text" size="60" value={this.state.newPostValue} onChange={evt => this.updateInputValue(evt)}></input><button onClick={() => this.addPost()}>POST</button>
+        </div>
+        {this.state.postValues.map(function(name) {
+          return <div style={{marginBottom:"10px"}}><Post value={name}/></div>;
+        })}
+      </div>
+    );
+  }
+
+  updateInputValue(evt) {
+    this.setState({
+      newPostValue: evt.target.value,
+    });
+  }
+
+  addPost() {
+    let valuesTemp = this.state.postValues;
+    valuesTemp.push(this.state.newPostValue);
+    this.setState({
+      postValues: valuesTemp,
+    });
+  }
+}
+
+
+class Post extends React.Component {
+  render() {
+    return (
+      <div style={{height:"100px", width:"500px", border: "2px solid lightblue", borderRadius:"5px"}}>
+        <div style={{height:"75px", verticalAlign:"middle"}}>{this.props.value}</div>
+        <div style={{border: "2px solid gray", borderRadius:"5px"}}>
+          <span style={{marginRight:"10px"}}><a href="">Like</a></span>
+          <span style={{marginRight:"10px"}}><a href="">Comment</a></span>
+          <span><a href="">Share</a></span>
+        </div>
       </div>
     );
   }
